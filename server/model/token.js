0f1d28f8c6f2134cef000ca7c00370a1c69a9b52
Irfan Akhtar <irfangujjar3499@gmail.com>
2022-09-14 14:24:23 +0500

    first

---
+++ server/model/token.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+
+const TokenSchema = mongoose.Schema({
+    token: {
+        type: String,
+        required: true
+    }
+});
+module.exports = mongoose.model('token', TokenSchema);
+
